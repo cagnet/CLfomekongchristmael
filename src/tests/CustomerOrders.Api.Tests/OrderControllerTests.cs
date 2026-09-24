2658e52afd6635bb90a07c3f4dfa872c1d0c1d75
christ.mael <fomekongchristmael@gmail.com>
2026-09-24 08:49:21 +0100

    (test): add comprehensive OrderController tests for GET, DELETE, and PATCH endpoints

--- src/tests/CustomerOrders.Api.Tests/OrderControllerTests.cs
+++ src/tests/CustomerOrders.Api.Tests/OrderControllerTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using CustomerOrders.Business.Entities;
 using CustomerOrders.Business.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -14,6 +15,24 @@ public class OrderControllerTests(WebApplicationFactory<Program> webApplicationF
     private HttpClient _httpClient = webApplicationFactory.CreateClient();
 
 
+    [Fact]
+    public async Task GetOneOrder_WhenOrderNotExists_ReturnNotFound()
+    {
+        const int id = 40000;
+        var response = await _httpClient.GetAsync($"orders/{id}");
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task GetOneOrder_WhenOrderExists_ReturnOk()
+    {
+        int customerId = await CreateCustomer(GenerateRandomString(8), true);
+        Order order = await CreateOrder(customerId, 6000);
+        var response = await _httpClient.GetAsync($"orders/{order.Id}");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    
     [Fact]
     public async Task CreateOrder_WhenCustomerNotExists_Return400BadRequest()
     {
@@ -75,6 +94,56 @@ public class OrderControllerTests(WebApplicationFactory<Program> webApplicationF
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
 
+    [Fact]
+    public async Task DeleteOrder_WhenOrderExists_ReturnNoContent()
+    {
+        int customerId = await CreateCustomer(GenerateRandomString(8), true);
+        Order order = await CreateOrder(customerId, 100);
+
+        var response = await _httpClient.DeleteAsync($"/orders/{order.Id}");
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        
+    }
+    
+    [Fact]
+    public async Task DeleteOrder_WhenOrderNotExists_ReturnNotFound()
+    {
+        int customerId = await CreateCustomer(GenerateRandomString(8), true);
+        Order order = await CreateOrder(customerId, 100);
+
+        var response = await _httpClient.DeleteAsync($"/orders/{order.Id + 10000}");
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        
+    }
+    
+    [Fact]
+    public async Task UpdateOrder_WhenOrderNotExists_ReturnNotFound()
+    {
+        int customerId = await CreateCustomer(GenerateRandomString(8), true);
+        Order order = await CreateOrder(customerId, 100);
+
+        var response = await PatchAsync(_httpClient, $"/orders/{order.Id + 10000}", new UpdateOrder(Amount: 200));
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        
+    }
+    
+    [Fact]
+    public async Task UpdateOrder_WhenOrderExists_ReturnOk()
+    {
+        int customerId = await CreateCustomer(GenerateRandomString(8), true);
+        Order order = await CreateOrder(customerId, 100);
+
+        const int newAmount = 200;
+        var response = await PatchAsync(_httpClient, $"/orders/{order.Id}", new UpdateOrder(Amount: newAmount));
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var body = await response.Content.ReadFromJsonAsync<Order>();
+        Assert.NotNull(body);
+        Assert.Equal(newAmount, body.Amount);
+
+    }
+
 
     private async Task<int> CreateCustomer(String name, bool isActive)
     {
@@ -92,6 +161,18 @@ public class OrderControllerTests(WebApplicationFactory<Program> webApplicationF
 
     }
 
+    private async Task<Order> CreateOrder(int customerId, decimal amount)
+    {
+        var response = await _httpClient.PostAsJsonAsync($"/customers/{customerId}/orders", new
+        {
+            Amount = 10
+        });
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Order? order =  await response.Content.ReadFromJsonAsync<Order>();
+        Assert.NotNull(order);
+        return order;
+    }
+
     private String GenerateRandomString(int length)
     {
         string alphabet = "qwertyuiopasdfghjklzxcvbnm";
@@ -104,5 +185,14 @@ public class OrderControllerTests(WebApplicationFactory<Program> webApplicationF
         }
 
         return new string(words);
+    }
+
+    private async Task<HttpResponseMessage> PatchAsync<T>(HttpClient client, String url, T content)
+    {
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Patch, url)
+        {
+            Content = JsonContent.Create(content)
+        };
+        return await client.SendAsync(request);
     }
 }
