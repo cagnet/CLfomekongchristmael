5b0926c694de364c5d130bbcfeca5f9acd596916
christ.mael <fomekongchristmael@gmail.com>
2026-09-24 09:39:05 +0100

    (test): add conflict response tests for customer creation and deletion scenarios
    - Added tests to verify conflict responses when creating a customer with an existing email.
    - Implemented a test to ensure deletion fails with conflict if the customer owns orders.
    - Extended `HttpHelpers` with reusable methods for creating customers and orders.

--- src/tests/CustomerOrders.Api.Tests/CustomerControllerTests.cs
+++ src/tests/CustomerOrders.Api.Tests/CustomerControllerTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using CustomerOrders.Api.Tests.Helpers;
 using CustomerOrders.Business.Entities;
+using CustomerOrders.Business.Exceptions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace CustomerOrders.Api.Tests;
@@ -11,6 +12,20 @@ public class CustomerControllerTests(
     ): IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _httpClient = webApplicationFactory.CreateClient();
+
+    [Fact]
+    public async Task CreateCustomer_WhenCustomerWithSameEmailExists_ReturnConflict()
+    {
+        var createdCustomer = await  HttpHelpers.CreateAndGetCustomer(_httpClient, "IsMa", false);
+        var response = await _httpClient.PostAsJsonAsync("/customers", new CreateCustomer(
+            "Isabelle", "Christine", createdCustomer.Email, "Yaounde", true
+        ));
+        
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        var body = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(body);
+        Assert.Contains(BusinessRuleCodes.Conflicts.EmailAlreadyTaken, body);
+    }
     
     [Fact]
     public async Task GetOneCustomer_WhenCustomerNotExists_ReturnNotFound()
@@ -44,10 +59,25 @@ public class CustomerControllerTests(
     {
         int customerId = await HttpHelpers.CreateCustomer(_httpClient, HttpHelpers.GenerateRandomString(8), true);
 
-        var response = await _httpClient.DeleteAsync($"/Customers/{customerId + 10000}");
+        var response = await _httpClient.DeleteAsync($"/customers/{customerId + 10000}");
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         
     }
+    
+    [Fact]
+    public async Task DeleteCustomer_WhenCustomerOwnsOrders_ReturnConflict()
+    {
+        int customerId = await HttpHelpers.CreateCustomer(_httpClient, HttpHelpers.GenerateRandomString(8), true);
+        Order order = await HttpHelpers.CreateOrder(_httpClient, customerId, 100);
+
+        var response = await _httpClient.DeleteAsync($"/customers/{customerId}");
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        var body = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(body);
+        Assert.Contains(BusinessRuleCodes.Conflicts.CustomerHasOrders, body);
+        
+    }
+    
     
     [Fact]
     public async Task UpdateCustomer_WhenCustomerNotExists_ReturnNotFound()
--- src/tests/CustomerOrders.Api.Tests/Helpers/HttpHelpers.cs
+++ src/tests/CustomerOrders.Api.Tests/Helpers/HttpHelpers.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using CustomerOrders.Business.Entities;
 
 namespace CustomerOrders.Api.Tests.Helpers;
 
@@ -22,6 +23,22 @@ public static class HttpHelpers
 
     }
     
+    public static async Task<Customer> CreateAndGetCustomer(HttpClient client, String name, bool isActive, string email = null)
+    {
+        var createCustomerRes = await client.PostAsJsonAsync("/customers", new CreateCustomer(
+            Name: name,
+            IsActive: isActive,
+            FirstName: GenerateRandomString(6),
+            Email: email ?? $"{GenerateRandomString(10)}@gmail.com",
+            Address: GenerateRandomString(9)
+        ));
+        Assert.Equal(HttpStatusCode.Created, createCustomerRes.StatusCode);
+        var body = await createCustomerRes.Content.ReadFromJsonAsync<Customer>();
+        Assert.NotNull(body);
+        return body;
+
+    }
+    
     public static String GenerateRandomString(int length)
     {
         string alphabet = "qwertyuiopasdfghjklzxcvbnm";
@@ -34,6 +51,18 @@ public static class HttpHelpers
         }
 
         return new string(words);
+    }
+    
+    public static async Task<Order> CreateOrder(HttpClient client, int customerId, decimal amount)
+    {
+        var response = await client.PostAsJsonAsync($"/customers/{customerId}/orders", new
+        {
+            Amount = 10
+        });
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Order? order =  await response.Content.ReadFromJsonAsync<Order>();
+        Assert.NotNull(order);
+        return order;
     }
     
     public static async Task<HttpResponseMessage> PatchAsync<T>(HttpClient client, String url, T content)
